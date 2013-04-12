6b7b414b56a2bc5d67d794d4cac44de7fdf809a5
nunezro2 <nunezro2@illinois.edu>
2013-04-12 17:58:29 -0500

    Creado main y clase node

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Creating the graph
+		UndirectedGraph<String, DefaultEdge> graph = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+		
+		
+	}
+
+}
---
+++ src/node.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class node {
+
+	private final String type;
+	private long id;
+	
+	public node(String type, long id ) {
+		
+		this.type = type;
+		this.id = id;
+	}
+}
